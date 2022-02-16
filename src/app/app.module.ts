485fd075588aa80dbcb52683fd47cc392b64ce0e
Villegas545 <villegas_545@hotmail.com>
2022-02-16 11:35:00 -0700

    setup firebase complete setup material complete setup ngrx complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angulartest-8f3a0',
+    appId: '1:297279002272:web:7e6dbe1b5bc447e70c9bc5',
+    storageBucket: 'angulartest-8f3a0.appspot.com',
+    apiKey: 'AIzaSyC1SnGugLu7a8ulQETi1tasRdGitQZRvvo',
+    authDomain: 'angulartest-8f3a0.firebaseapp.com',
+    messagingSenderId: '297279002272',
+    measurementId: 'G-56Y6E8VTMH',
+  },
   production: true
 };
