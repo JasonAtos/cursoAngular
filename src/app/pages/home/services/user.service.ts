2f8701e89164eb08b8b32e4f7ccdfdba16575209
Villegas545 <villegas_545@hotmail.com>
2022-02-17 13:13:37 -0700

    mistake: try to apply the firestore collection

--- src/app/pages/home/services/user.service.ts
+++ src/app/pages/home/services/user.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { User } from '../../../models/user';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  CollectionReference,
+  DocumentData,
+} from '@angular/fire/firestore';
+
 /*  import { User } from '@models/user'; */
+export interface Item {
+  name: string;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private mockUsers: User[] = [
+  /*   private mockUsers: User[] = [
     {
       id: 1,
       name: 'John Doe',
@@ -19,9 +30,18 @@ export class UserService {
       email: 'jhon@gmail.com',
       password: '123456',
     },
-  ];
-  constructor() {}
+  ]; */
+  public item$!: Observable<User[]>;
+  public collection2: any;
+
+  constructor(firestore: Firestore) {
+    /* this.collection2 = firestore.collection<User>(firestore, 'Users'); */
+    /* collectionData(this.collection2).subscribe(console.log); */
+    this.collection2 = collection(firestore, 'items');
+    this.item$ = collectionData(this.collection2);
+  }
   public getUsers(): Observable<User[]> {
-    return of(this.mockUsers);
+    this.item$ = collectionData(this.collection2);
+    return this.item$;
   } // getUsers
 }
