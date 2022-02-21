fa2eb6cabcfc19d28d9f244b96967f259fc86046
Jason Bañuelos <jason-eduardo.banuelos@atos.net>
2022-02-21 17:39:30 -0600

    firebase configurado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBKZ71o76O4_Fq5ozGIxA9qauNcy_yxccc",
+    authDomain: "restaurant-6efa4.firebaseapp.com",
+    projectId: "restaurant-6efa4",
+    storageBucket: "restaurant-6efa4.appspot.com",
+    messagingSenderId: "148369826333",
+    appId: "1:148369826333:web:3be5872b8546161d651cf4",
+    measurementId: "G-Z2MLNM1G2S"
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
