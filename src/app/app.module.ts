ca3bde7872fda4d8421fee73a52df32df5e72a5c
eltortillero <horeb@live.com.mx>
2022-02-22 09:54:20 -0700

    reducer importation, and index reconfiguration, changed the content from app.component view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { APP_STATE_REDUCERS } from './state/state';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     StoreModule.forRoot(APP_STATE_REDUCERS),
     StoreDevtoolsModule.instrument({ name:"debug area" }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/state/state.ts
+++ src/app/state/state.ts
@@ -1,6 +1,7 @@
 import { OrderState, UserState } from "./state_models/state.models";
 import { ActionReducerMap } from "@ngrx/store";
 import { UserReducer } from "./reducers/user.reducers";
+import { OrderReducer } from "./reducers/order.reducer";
 
 export interface AppState {
   user:UserState;
